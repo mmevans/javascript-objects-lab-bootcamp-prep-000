10d591f2fed6feb6696d107c1a3cd32ed975e288
Michael Evans <mmevansjr@gmail.com>
2019-03-31 17:17:57 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,16 @@
+var recipes = {};
+
+function updateObjectWithKeyAndValue(recipes, eggs, one) {
+  
+  return Object.assign({}, recipes, {[eggs]: one});
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(recipes, flour, two) {
+  recipes[flour] = two;
+  
+  return recipes
+}
+  
+function deleteFromObjectByKey(recipes, flour) {
+  
+}
